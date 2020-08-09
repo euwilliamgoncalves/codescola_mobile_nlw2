138034ef76c1d0e9c9e7656db7c5832acb26fc8b
William Goncalves <euwilliamgoncalves@gmail.com>
2020-08-09 08:23:47 -0300

    Estilização do TeacherItem

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import styles from './styles';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+
+function TeacherItem() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.profile}>
+                <Image
+                    style={styles.avatar}
+                    source={{ uri: 'https://github.com/euwilliamgoncalves.png' }}
+                />
+                <View style={styles.profileInfo}>
+                    <Text style={styles.name}>William Gonçalves</Text>
+                    <Text style={styles.subject}>UI/UX</Text>
+                </View>    
+            </View>
+
+            <Text style={styles.bio}>
+            Desenvolvedor FullStack em aprendizado, apaixonado por Front-End, Design, UI e UX. Pai, namorido, carioca e flamenguista que já sonhou em ser fotógrafo.
+            </Text>
+
+            <View style={styles.footer}>
+                <Text style={styles.price}>
+                    Preço/hora {'   '}
+                    <Text style={styles.priceValue}>R$ 50</Text> 
+                </Text>
+                <View style={styles.buttonsContainer}>
+                    <RectButton style={styles.favoriteButton}>
+                        <Image source={heartOutlineIcon} />
+                    </RectButton>
+                    <RectButton style={styles.contactButton}>
+                        <Image source={whatsappIcon} />
+                        <Text style={styles.contactButtonText}>Entrar em contato</Text>
+                    </RectButton>
+                </View>
+            </View>
+
+            
+
+        </View>
+    )
+}
+
+export default TeacherItem;
---
+++ src/components/TeacherItem/styles.ts
@@ -0,0 +1,105 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: '#4caac2',
+        borderRadius: 8,
+        marginHorizontal: 16,
+        marginTop: -16,
+        marginBottom: 32,
+        overflow: 'hidden'
+    },
+
+    profile: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 24,
+    },
+
+    avatar: {
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        backgroundColor: '#eee'
+    },
+
+    profileInfo: {
+        marginLeft: 16,
+    },
+
+    name: {
+        fontFamily: 'Play_700Bold',
+        fontSize: 20,
+        color: '#135764'
+    },
+
+    subject: {
+        fontFamily: 'Montserrat_400Regular',
+        color: '#6A6180',
+        fontSize: 12,
+        marginTop: 4
+    },
+    bio: {
+        marginHorizontal: 24,
+        fontFamily: 'Montserrat_400Regular',
+        fontSize: 14,
+        lineHeight: 28,
+        color: '#6A6180',
+    },
+
+    footer: {
+        backgroundColor: '#fafafc',
+        padding: 24,
+        alignItems: 'center',
+        marginTop: 24
+    },
+
+    price: {
+        fontFamily: 'Montserrat_400Regular',
+        color: '#0f404b',
+        fontSize: 14,
+    },
+    
+    priceValue: {
+        fontFamily: 'Play_700Bold',
+        color: '#135764',
+        fontSize: 16,
+    },
+
+    buttonsContainer: {
+        flexDirection: 'row',
+        marginTop: 16,
+    },
+
+    favoriteButton: {
+        backgroundColor: '#16697A',
+        width: 56,
+        height: 56,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 8
+    },
+
+    contactButton: {
+        backgroundColor: '#FFA62B',
+        flex: 1,
+        height: 56,
+        borderRadius: 8,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 8,
+    },
+
+    contactButtonText: {
+        color: '#fff',
+        fontFamily: 'Play_700Bold',
+        fontSize: 16,
+        marginLeft: 16
+    }
+});
+
+export default styles;
