439d66c307f18d1614400bca2c20b641c40181e4
William Goncalves <euwilliamgoncalves@gmail.com>
2020-08-09 04:54:44 -0300

    Iniciando rotas

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { AppLoading } from 'expo';
 
-import Landing from './src/pages/Landing';
+import AppStack from './src/routes/AppStack';
 
 import { Montserrat_400Regular, Montserrat_600SemiBold, useFonts } from '@expo-google-fonts/montserrat';
 import { Play_400Regular, Play_700Bold } from '@expo-google-fonts/play';
@@ -20,7 +20,7 @@ export default function App() {
   }else{
     return (
       <>
-        <Landing />
+        <AppStack />
         <StatusBar style="light" />  
       </>
     );
